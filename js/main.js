b9260463baa2a80cac4a920913763b2622f3945a
sharteeya <abcdok77@gmail.com>
2022-07-13 10:08:43 +0800

    style: clean up codes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-unused-vars */
-/* eslint-disable no-param-reassign */
 const COMMAND_TYPE = {
     SWORD: 400,
     ARCH: 300,
@@ -22,7 +21,14 @@ const TAP_MODE = {
     EDITING: 100,
 };
 
-let searchStatus = 0;
+const ROW_MIN_INDEX = 0;
+const ROW_MAX_INDEX = 4;
+const ROW_LENGTH = 5;
+const COL_MIN_INDEX = 0;
+const COL_MAX_INDEX = 5;
+const COL_LENGTH = 6;
+
+let searchStep = 0;
 let tapMode = TAP_MODE.PLAYING;
 let editCommand = COMMAND_TYPE.SWORD;
 
@@ -42,8 +48,8 @@ const healCount = document.getElementById('healCount');
 const dropResult = document.getElementById('dropResult');
 const editBtns = [editSword, editArch, editMagic, editHeal, editInvalid, editStone];
 
-const pane = new Array(5).fill(0).map(() => new Array(6).fill(0));
-const searchPane = new Array(5).fill(0).map(() => new Array(6).fill(0));
+const pane = new Array(ROW_LENGTH).fill(0).map(() => new Array(COL_LENGTH).fill(0));
+const searchPane = new Array(ROW_LENGTH).fill(0).map(() => new Array(COL_LENGTH).fill(0));
 
 const changeMode = (mode) => {
     tapMode = mode;
@@ -51,8 +57,8 @@ const changeMode = (mode) => {
 
 const changeEdit = (editCmd) => {
     editCommand = editCmd;
-    editBtns.forEach((b) => {
-        b.classList.add('cmd-unselect');
+    editBtns.forEach((btn) => {
+        btn.classList.add('cmd-unselect');
     });
     switch (editCmd) {
     case COMMAND_TYPE.SWORD:
@@ -98,71 +104,69 @@ const getCommandImagePath = (command) => {
 };
 
 const refreshPane = () => {
-    searchStatus = 0;
-    Array(5).fill(0).forEach((_, row) => {
-        Array(6).fill(0).forEach((__, col) => {
+    searchStep = 0;
+    for (let row = ROW_MIN_INDEX; row <= ROW_MAX_INDEX; row += 1) {
+        for (let col = COL_MIN_INDEX; col <= COL_MAX_INDEX; col += 1) {
             pane[row][col] = (Math.floor(Math.random() * 4) + 1) * 100;
             searchPane[row][col] = SEARCH_TYPE.EMPTY;
-        });
-    });
+        }
+    }
 };
 
 const refreshPaneImage = () => {
-    let newPane = '';
-    Array(5).fill(0).forEach((_, row) => {
-        Array(6).fill(0).forEach((__, col) => {
-            const path = getCommandImagePath(pane[row][col]);
-            newPane += `<div class="pane-grid
-                            ${searchPane[row][col] > SEARCH_TYPE.START ? 'pane-grid-onselect' : ''}
-                            ${searchPane[row][col] === SEARCH_TYPE.START ? 'pane-grid-onselect-start' : ''}"
-                            onClick="clickCommand(${row}, ${col});
-                        ">
-                        <img src="${path}" class="img-fluid">
-                        ${searchPane[row][col] >= SEARCH_TYPE.START ? `<div class="command-number">${searchPane[row][col] - SEARCH_TYPE.START + 1}</div>` : ''}
-                        </div>`;
-        });
-    });
-    paneDisplay.innerHTML = newPane;
+    let newPaneHTML = '';
+    for (let row = ROW_MIN_INDEX; row <= ROW_MAX_INDEX; row += 1) {
+        for (let col = COL_MIN_INDEX; col <= COL_MAX_INDEX; col += 1) {
+            newPaneHTML += `
+            <div class="pane-grid ${searchPane[row][col] > SEARCH_TYPE.START ? 'pane-grid-onselect' : ''}
+            ${searchPane[row][col] === SEARCH_TYPE.START ? 'pane-grid-onselect-start' : ''}" onClick="clickCommand(${row}, ${col});">
+            <img src="${getCommandImagePath(pane[row][col])}" class="img-fluid">
+            ${searchPane[row][col] >= SEARCH_TYPE.START ? `<div class="command-number">${searchPane[row][col] - SEARCH_TYPE.START + 1}</div>` : ''}
+            </div>`;
+        }
+    }
+    paneDisplay.innerHTML = newPaneHTML;
 };
 
-const refreshSearchPane = () => {
-    pane.forEach((rows, row) => {
-        rows.forEach((value, col) => {
-            if (value === COMMAND_TYPE.STONE) {
+const resetSearchPane = () => {
+    for (let row = ROW_MIN_INDEX; row <= ROW_MAX_INDEX; row += 1) {
+        for (let col = COL_MIN_INDEX; col <= COL_MAX_INDEX; col += 1) {
+            if (pane[row][col] === COMMAND_TYPE.STONE) {
                 searchPane[row][col] = SEARCH_TYPE.UNSELECTABLE;
             } else {
                 searchPane[row][col] = SEARCH_TYPE.EMPTY;
             }
-        });
-    });
+        }
+    }
 };
 
-const updateSearchPane = (row, col) => {
-    if (searchPane[row][col] === SEARCH_TYPE.EMPTY) {
-        searchPane[row][col] = SEARCH_TYPE.START + searchStatus;
-        searchStatus += 1;
-    } else if (searchPane[row][col] >= SEARCH_TYPE.START) {
-        const calcelNumber = searchPane[row][col];
+const updateSearchPane = (pos) => {
+    if (searchPane[pos.row][pos.col] === SEARCH_TYPE.EMPTY) {
+        searchPane[pos.row][pos.col] = SEARCH_TYPE.START + searchStep;
+        searchStep += 1;
+    } else if (searchPane[pos.row][pos.col] >= SEARCH_TYPE.START) {
+        const calcelNumber = searchPane[pos.row][pos.col];
         let calcelCount = 0;
-        searchPane.forEach((rows, rowIdx) => {
-            rows.forEach((value, colIdx) => {
-                if (value >= calcelNumber) {
-                    searchPane[rowIdx][colIdx] = SEARCH_TYPE.EMPTY;
+        for (let row = ROW_MIN_INDEX; row <= ROW_MAX_INDEX; row += 1) {
+            for (let col = COL_MIN_INDEX; col <= COL_MAX_INDEX; col += 1) {
+                if (searchPane[row][col] === calcelNumber) {
+                    searchPane[row][col] = SEARCH_TYPE.EMPTY;
                     calcelCount += 1;
-                }
-                if (value > calcelNumber) {
-                    switch (pane[rowIdx][colIdx]) {
+                } else if (searchPane[row][col] > calcelNumber) {
+                    searchPane[row][col] = SEARCH_TYPE.EMPTY;
+                    calcelCount += 1;
+                    switch (pane[row][col]) {
                     case COMMAND_TYPE.SWORD:
-                        pane[rowIdx][colIdx] = COMMAND_TYPE.ARCH;
+                        pane[row][col] = COMMAND_TYPE.ARCH;
                         break;
                     case COMMAND_TYPE.ARCH:
-                        pane[rowIdx][colIdx] = COMMAND_TYPE.SWORD;
+                        pane[row][col] = COMMAND_TYPE.SWORD;
                         break;
                     case COMMAND_TYPE.MAGIC:
-                        pane[rowIdx][colIdx] = COMMAND_TYPE.HEAL;
+                        pane[row][col] = COMMAND_TYPE.HEAL;
                         break;
                     case COMMAND_TYPE.HEAL:
-                        pane[rowIdx][colIdx] = COMMAND_TYPE.MAGIC;
+                        pane[row][col] = COMMAND_TYPE.MAGIC;
                         break;
                     case COMMAND_TYPE.STONE:
                     case COMMAND_TYPE.POISON:
@@ -171,69 +175,70 @@ const updateSearchPane = (row, col) => {
                         break;
                     }
                 }
-            });
-        });
+            }
+        }
         refreshPaneImage();
-        searchStatus -= calcelCount;
+        searchStep -= calcelCount;
     }
 };
 
-const clickCommand = (gridRow, gridCol) => {
+const clickCommand = (row, col) => {
     if (tapMode === TAP_MODE.PLAYING) {
-        switch (pane[gridRow][gridCol]) {
+        switch (pane[row][col]) {
         case COMMAND_TYPE.SWORD:
-            pane[gridRow][gridCol] = COMMAND_TYPE.ARCH;
-            updateSearchPane(gridRow, gridCol);
+            pane[row][col] = COMMAND_TYPE.ARCH;
+            updateSearchPane({ row, col });
             break;
         case COMMAND_TYPE.ARCH:
-            pane[gridRow][gridCol] = COMMAND_TYPE.SWORD;
-            updateSearchPane(gridRow, gridCol);
+            pane[row][col] = COMMAND_TYPE.SWORD;
+            updateSearchPane({ row, col });
             break;
         case COMMAND_TYPE.MAGIC:
-            pane[gridRow][gridCol] = COMMAND_TYPE.HEAL;
-            updateSearchPane(gridRow, gridCol);
+            pane[row][col] = COMMAND_TYPE.HEAL;
+            updateSearchPane({ row, col });
             break;
         case COMMAND_TYPE.HEAL:
-            pane[gridRow][gridCol] = COMMAND_TYPE.MAGIC;
-            updateSearchPane(gridRow, gridCol);
+            pane[row][col] = COMMAND_TYPE.MAGIC;
+            updateSearchPane({ row, col });
             break;
         case COMMAND_TYPE.POISON:
         case COMMAND_TYPE.INVALID:
-            updateSearchPane(gridRow, gridCol);
+            updateSearchPane({ row, col });
             break;
         case COMMAND_TYPE.STONE:
         default:
             break;
         }
         refreshPaneImage();
-        stepCount.value = searchStatus;
+        stepCount.value = searchStep;
     } else if (tapMode === TAP_MODE.EDITING) {
-        pane[gridRow][gridCol] = editCommand;
+        pane[row][col] = editCommand;
         refreshPaneImage();
     }
 };
 
 const paneTidy = (currentPane) => {
-    for (let col = 0; col <= 5; col += 1) {
-        let colsCmd = [currentPane[4][col], currentPane[3][col], currentPane[2][col], currentPane[1][col], currentPane[0][col]];
-        colsCmd = colsCmd.filter((cmd) => cmd !== COMMAND_TYPE.EMPTY);
-        while (colsCmd.length > 0 && colsCmd[0] === COMMAND_TYPE.STONE) {
-            colsCmd.shift();
+    const newPane = currentPane.map((r) => r.slice());
+    for (let col = COL_MIN_INDEX; col <= COL_MAX_INDEX; col += 1) {
+        let colCommands = [newPane[4][col], newPane[3][col], newPane[2][col], newPane[1][col], newPane[0][col]];
+        colCommands = colCommands.filter((cmd) => cmd !== COMMAND_TYPE.EMPTY);
+        while (colCommands.length > 0 && colCommands[0] === COMMAND_TYPE.STONE) {
+            colCommands.shift();
         }
-        if (colsCmd.length < 5) {
-            colsCmd = colsCmd.concat(Array(5 - colsCmd.length).fill(0));
+        if (colCommands.length < 5) {
+            colCommands = colCommands.concat(Array(5 - colCommands.length).fill(0));
         }
-        for (let row = 4; row >= 0; row -= 1) {
-            currentPane[row][col] = colsCmd[4 - row];
+        for (let row = ROW_MAX_INDEX; row >= ROW_MIN_INDEX; row -= 1) {
+            newPane[row][col] = colCommands[4 - row];
         }
     }
-    return currentPane;
+    return newPane;
 };
 
 const findSameCommand = (row, col, inputPane) => {
     const currentPane = inputPane.map((r) => r.slice());
     const processing = [{ row, col }];
-    const tobeRemoved = [];
+    const matches = [];
     const targetCommand = currentPane[row][col];
     while (processing.length > 0) {
         const target = processing.shift();
@@ -243,53 +248,53 @@ const findSameCommand = (row, col, inputPane) => {
             if (currentPane[currentRow - 1][currentCol] === targetCommand) {
                 processing.push({ row: currentRow - 1, col: currentCol });
             } else if (currentPane[currentRow - 1][currentCol] === COMMAND_TYPE.INVALID) {
-                tobeRemoved.push({ row: currentRow - 1, col: currentCol });
+                matches.push({ row: currentRow - 1, col: currentCol });
             }
         }
         if (currentRow < 4) {
             if (currentPane[currentRow + 1][currentCol] === targetCommand) {
                 processing.push({ row: currentRow + 1, col: currentCol });
             } else if (currentPane[currentRow + 1][currentCol] === COMMAND_TYPE.INVALID) {
-                tobeRemoved.push({ row: currentRow + 1, col: currentCol });
+                matches.push({ row: currentRow + 1, col: currentCol });
             }
         }
         if (currentCol > 0) {
             if (currentPane[currentRow][currentCol - 1] === targetCommand) {
                 processing.push({ row: currentRow, col: currentCol - 1 });
             } else if (currentPane[currentRow][currentCol - 1] === COMMAND_TYPE.INVALID) {
-                tobeRemoved.push({ row: currentRow, col: currentCol - 1 });
+                matches.push({ row: currentRow, col: currentCol - 1 });
             }
         }
         if (currentCol < 5) {
             if (currentPane[currentRow][currentCol + 1] === targetCommand) {
                 processing.push({ row: currentRow, col: currentCol + 1 });
             } else if (currentPane[currentRow][currentCol + 1] === COMMAND_TYPE.INVALID) {
-                tobeRemoved.push({ row: currentRow, col: currentCol + 1 });
+                matches.push({ row: currentRow, col: currentCol + 1 });
             }
         }
-        tobeRemoved.push({ row: currentRow, col: currentCol });
+        matches.push({ row: currentRow, col: currentCol });
         currentPane[currentRow][currentCol] = COMMAND_TYPE.EMPTY;
     }
     return {
         targetCommand,
-        tobeRemoved,
+        matches,
     };
 };
 
 const findCombo = (inputPane) => {
-    const currentPane = inputPane.map((r) => r.slice()); const
-        commandCount = {
-            sword: 0, arch: 0, magic: 0, heal: 0,
-        };
+    const currentPane = inputPane.map((r) => r.slice());
+    const commandCount = {
+        sword: 0, arch: 0, magic: 0, heal: 0,
+    };
     let combo = 0;
-    for (let row = 0; row <= 4; row += 1) {
-        for (let col = 0; col <= 4; col += 1) {
+    for (let row = ROW_MIN_INDEX; row <= ROW_MAX_INDEX; row += 1) {
+        for (let col = COL_MIN_INDEX; col <= COL_MAX_INDEX - 2; col += 1) {
             if (currentPane[row][col] === currentPane[row][col + 1]
                 && currentPane[row][col] === currentPane[row][col + 2]
                 && currentPane[row][col] > COMMAND_TYPE.EMPTY) {
                 const result = findSameCommand(row, col, currentPane);
                 let count = 0;
-                result.tobeRemoved.forEach((pos) => {
+                result.matches.forEach((pos) => {
                     if (currentPane[pos.row][pos.col] === result.targetCommand) {
                         count += 1;
                     }
@@ -316,14 +321,14 @@ const findCombo = (inputPane) => {
             }
         }
     }
-    for (let col = 0; col <= 5; col += 1) {
-        for (let row = 0; row <= 2; row += 1) {
+    for (let col = COL_MIN_INDEX; col <= COL_MAX_INDEX; col += 1) {
+        for (let row = ROW_MIN_INDEX; row <= ROW_MAX_INDEX - 2; row += 1) {
             if (currentPane[row][col] === currentPane[row + 1][col]
                 && currentPane[row][col] === currentPane[row + 2][col]
                 && currentPane[row][col] > COMMAND_TYPE.EMPTY) {
                 const result = findSameCommand(row, col, currentPane);
                 let count = 0;
-                result.tobeRemoved.forEach((pos) => {
+                result.matches.forEach((pos) => {
                     if (currentPane[pos.row][pos.col] === result.targetCommand) {
                         count += 1;
                     }
@@ -370,24 +375,20 @@ const calculateCombo = (inputPane = pane.map((r) => r.slice())) => {
             commandCount[key] += result.commandCount[key];
         });
         totalCombo += combo;
-        currentPane = result.currentPane;
-        currentPane = paneTidy(currentPane);
+        currentPane = paneTidy(result.currentPane);
         if (combo > 0) {
-            let newPane = `
+            let newPaneHTML = `
             <h4>【STEP ${step}】→ ${combo} Combo</h4>
             <p> 消除後的版面如下</p>
-            <div class="pane-wrapper">
-            `;
-            for (let row = 0; row < 5; row += 1) {
-                for (let col = 0; col < 6; col += 1) {
-                    const path = getCommandImagePath(currentPane[row][col]);
-                    newPane += `<div class="pane-grid">
-                                    <img src="${path}" class="img-fluid">
-                                </div>`;
+            <div class="pane-wrapper">`;
+
+            for (let row = ROW_MIN_INDEX; row <= ROW_MAX_INDEX; row += 1) {
+                for (let col = COL_MIN_INDEX; col <= COL_MAX_INDEX; col += 1) {
+                    newPaneHTML += `<div class="pane-grid"><img src="${getCommandImagePath(currentPane[row][col])}" class="img-fluid"></div>`;
                 }
             }
-            newPane += '</div><br>';
-            dropResult.innerHTML += newPane;
+            newPaneHTML += '</div><br>';
+            dropResult.innerHTML += newPaneHTML;
             step += 1;
         }
     } while (combo > 0);
@@ -402,7 +403,7 @@ const calculateCombo = (inputPane = pane.map((r) => r.slice())) => {
 const init = () => {
     refreshPane();
     refreshPaneImage();
-    refreshSearchPane();
+    resetSearchPane();
     dropResult.innerHTML = '';
     comboCount.value = '尚未計算';
     swordCount.value = '尚未計算';
